24c964565ea028db44ad24f38f15f751d95ddff5
Fábio <videiraft@gmail.com>
2019-08-15 17:40:38 +0200

    refactor: eliminate class for simplicity

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 /*
-Given a singly-linked list, reverse it iteratively.
+Implement the reverseIteratively function that reverses a singly-linked list.
+You can assume that you always receive an instance of ListNode as argument.
 Can you get a recursive solution?
-*/
 
 class ListNode {
   constructor(num) {
     this.val = num
     this.next = null
   }
-  // Helper function to print the list
-  printList() {
-    let node = this
-    let output = '' 
-    while (node != null) {
-      output += node.val + ' '
-      node = node.next
-    }
-    return output
-  }
+}
+Example: 
+Input: 4 -> 3 -> 2 -> 1 -> 0 -> null
+Output: 0 -> 1 -> 2 -> 3 -> 4 -> null
+*/
 
-  // Iterative Solution
-  reverseIteratively() {
-    // write your code here
-  }
 
-  // (Extra!) Recursive Solution      
-  // reverseRecursively() {
-  //   // write your code here
-  // }
+let reverseIteratively
+let reverseRecursively
+
+// Iterative Solution
+reverseIteratively = (head) => {
+  // write your code HERE
+
 }
 
-module.exports = ListNode
+// (Extra!) Recursive Solution   
+// reverseRecursively = (head) => {
+//   // write your code HERE
+//  
+// }
+
+module.exports = { reverseIteratively, reverseRecursively };
--- test.js
+++ test.js
@@ -1,6 +1,22 @@
 require('chai').should()
 
-const ListNode = require('./index.js')
+const { reverseIteratively, reverseRecursively } = require('./index.js')
+
+class ListNode {
+  constructor(num) {
+    this.val = num
+    this.next = null
+  }
+  printList() {
+    let node = this
+    let output = '' 
+    while (node != null) {
+      output += node.val + ' '
+      node = node.next
+    }
+    return output
+  }
+}
 
 // Initialize the test list:
 const testHead = new ListNode(4)
@@ -12,21 +28,27 @@ const node3 = new ListNode(1)
 node2.next = node3
 const testTail = new ListNode(0)
 node3.next = testTail
-// 4 3 2 1 0
+// 4 3 2 1 0 ()
+
+const testHead1 = new ListNode(4)
+const testTail1 = testHead1
+// 4 (just one node)
 
 describe('Reverse a linked list', function () {
   
   it('should reverse a linked list iteratively', function () {
-    testHead.reverseIteratively()
+    reverseIteratively(testHead)
     testTail.printList().should.equal('0 1 2 3 4 ')
     testHead.printList().should.equal('4 ')
-  });
+    testHead1.printList().should.equal(testTail1.printList())
+  })
 
-  if (testHead.reverseRecursively) {
+  if (reverseRecursively) {
     it('should reverse a linked list recursevely', () => {
-      testTail.reverseRecursively()
+      reverseRecursively(testTail)
       testHead.printList().should.equal('4 3 2 1 0 ')
       testTail.printList().should.equal('0 ')
+      testTail1.printList().should.equal(testHead1.printList())
     })
   }
 })
